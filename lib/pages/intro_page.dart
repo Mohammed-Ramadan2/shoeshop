78a0d46317d7db09f852c061f4235a07e6cc9e08
hiso0ka44 <princeofshadows1@gmail.com>
2024-09-22 02:44:47 +0300

    end of the day added images and fixed some flutter issues

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Column(
+         children: [
+          Image.asset('assets/images/nike-logo-49337.png',height: ,),
+         ],
+       ),
+    );
+  }
+}

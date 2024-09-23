0261448b7d2ec2c8fc8972085fe5b423aaa223f5
hiso0ka44 <princeofshadows1@gmail.com>
2024-09-24 00:34:43 +0300

    drawer done without functionality

--- lib/component/bottom_nav_bar.dart
+++ lib/component/bottom_nav_bar.dart
@@ -59,7 +59,53 @@ class _BottomNavBarState extends State<BottomNavBar> {
               icon: Icon(Icons.menu));
         }),
       ),
-      drawer: Drawer(),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[600],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/zeelogo.png',
+                  height: MediaQuery.of(context).size.height * 0.15,
+                  color: Colors.white,
+                ),
+                ListTile(
+                  leading: Icon(
+                    FluentIcons.home_24_filled,
+                    color: Colors.white,
+                  ),
+                  title: Text(
+                    'H O M E',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    FluentIcons.book_exclamation_mark_20_filled,
+                    color: Colors.white,
+                  ),
+                  title:
+                      Text('A B O U T', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    FluentIcons.sign_out_20_filled,
+                    color: Colors.white,
+                  ),
+                  title: Text('S I G N   O U T',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
       body: screens[selectedIndex],
     );
   }
